ec031185d5da0741eb1c62fee96325e2f6861a2e
Boernsen <lernentobo@gmail.com>
2023-07-13 12:12:45 +0200

    04 Beispiele - Berechnung*

--- script_01.js
+++ script_01.js
@@ -34,10 +34,40 @@
 
 // JS ist eine untypisierte Sprache ! 
 
-let test;
-test = "Maxine";
-// test = 2;
-// test = false;
+// let test;
+// test = "Maxine";
+// // test = 2;
+// // test = false;
 
-console.log ("Datentype"+ typeof test);
-console.log ("Inhalt:" + test);
+// console.log ("Datentype"+ typeof test);
+// console.log ("Inhalt:" + test);
+
+// const firstName = "Max" ;  // Vabriable
+// firstName = "Maxine";
+// console.log (firstName); // Ausgabe
+
+/* ++++++++++ 04 Beispiele - Berechnung******/
+
+// Deklartion
+let ageJohn, ageMark;
+const birthYearJohn = 2000;
+const bithYearMark = 1990;
+
+let date = new Date();
+let year = date.getFullYear(); 
+ageJohn = 2023 - birthYearJohn;
+ageMark = 2023 - bithYearMark;
+
+
+console.log("ageJohne: " + ageJohn);
+console.log("ageMark: " + ageMark);
+
+
+// let date = new Date();
+// let year = date.getFullYear(); 
+// console.log(date);
+// console.log(year);
+
+
+
+
