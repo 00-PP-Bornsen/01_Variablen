303047594c62360a0dd1eea55a300d4dc2153846
Boernsen <lernentobo@gmail.com>
2023-07-13 11:03:23 +0200

    03 Deklaration + Wertzuweisung II

--- script_01.js
+++ script_01.js
@@ -12,13 +12,32 @@
 // console.log("Hi");
 // console.log(firstName);
 
-
 /***** 02 Deklaration + Wertzuweisung I *******/
 
-let firstName; // Deklaration (Definition)
-firstName = "Max"; //Wertzuweisung
-console.log (firstName); // Ausgabe 
+// let firstName; // Deklaration (Definition)
+// firstName = "Max"; //Wertzuweisung
+// console.log (firstName); // Ausgabe 
 
-let familyName;
-familyName = "Mütze";
-console.log(familyName);
+// let familyName;
+// familyName = "Mütze";
+// console.log(familyName);
+
+
+/************Deklaration + Wertzuweisung II **********/
+
+// let firstName, familyName ;
+
+// firstName = prompt("Bitte Vornamen eingeben:");
+// familyName = prompt("Bitte Nachhnamen eingeben:");
+
+// console.log (firstName + " " + familyName);
+
+// JS ist eine untypisierte Sprache ! 
+
+let test;
+test = "Maxine";
+// test = 2;
+// test = false;
+
+console.log ("Datentype"+ typeof test);
+console.log ("Inhalt:" + test);
